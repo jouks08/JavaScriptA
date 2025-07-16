875ce1656594b388b94b0afbc4a5764fcf98a323
jouks08 <joaquimipvargas@gmail.com>
2025-07-16 12:32:33 -0300

    aula 16/07

--- exercicios/script.js
+++ exercicios/script.js
@@ -26,5 +26,111 @@ function verificarIdade(){
 }
 
 function contarCaracteres(){
-    
+    let texto1 = document.getElementById("caracteres").value;
+    let textoTamanho = texto1.length;
+    document.getElementById("resultadoCaracteres").innerText = "resultadoCaracteres" + (textoTamanho);
+
 }
+
+function verificarParImpar(){
+
+    let num3 = Number(document.getElementById("num3").value);
+    let resultadoParImpar;
+
+    if(num3 % 2 == 0){
+       
+        document.getElementById("resultadoParImpar").innerText = "Seu número é par";
+
+    }
+
+    else{
+        
+        document.getElementById("resultadoParImpar").innerText = "Seu número é ímpar";
+    }
+
+}
+
+function converterMaiuscula(){
+
+    let texto2 = document.getElementById("nome").value;
+
+    document.getElementById("resultadoLetrasMaiusculas").innerText = (texto2).toUpperCase();
+
+
+}
+
+function verificarMaior(){
+
+    let num4 = document.getElementById("num4").value;
+    let num5 = document.getElementById("num5").value;
+
+    if(num4 > num5){
+
+        document.getElementById("resultadoMaior").innerText = (num4) + " é maior";
+
+    }
+
+    else if(num5 > num4){
+
+        document.getElementById("resultadoMaior").innerText = (num5) + " é maior"
+
+    }
+
+    else{
+        document.getElementById("resultadoMaior").innerText = "Os números são iguais"
+    }
+}
+
+function imprimirTabuada(){
+
+    let numTabuada = Number(document.getElementById("numTabuada").value);
+    let tabuada = "";
+
+    for(let i =1;i <= 10; i++){
+
+        tabuada += numTabuada + " x " + i + " = " + (numTabuada * i) + "\n";
+
+    }
+
+    document.getElementById("resultadoTabuada").innerText = tabuada;
+
+}
+
+function imprimirContagem(){
+
+    let contagem = "";
+
+    for(let i =1;i <= 10; i++){
+
+        contagem += i + "\n";
+
+    }
+
+    document.getElementById("contagem").innerText = contagem;
+
+
+
+}
+
+function verificarAprovacao(){
+
+    let nota1 = parseFloat(document.getElementById("nota1").value);
+    let nota2 = parseFloat(document.getElementById("nota2").value);
+    let nota3 = parseFloat(document.getElementById("nota3").value);
+
+    let media = (nota1 + nota2 + nota3) / 3;
+
+    if(media >= 6){
+
+        document.getElementById("aprovacao").innerText = "Aprovado com média: " + media.toFixed(2);
+
+    }
+
+    else {
+        document.getElementById("aprovacao").innerText = "Reprovado com média: " + media.toFixed(2);
+
+    }
+
+}
+
+
